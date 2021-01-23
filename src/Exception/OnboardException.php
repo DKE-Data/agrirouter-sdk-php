cb193fece07724183510f9c4bfe5b4f76b55e4cb
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-01-23 07:01:36 +0100

    Add comments.

--- src/Exception/OnboardException.php
+++ src/Exception/OnboardException.php
@@ -3,15 +3,20 @@
 namespace App\Exception;
 
 use Exception;
+use JetBrains\PhpStorm\Pure;
 
+/**
+ * Will be thrown if there is an error during the onboarding process.
+ * @package App\Exception
+ */
 class OnboardException extends Exception
 {
-    public function __construct($message, $code = 0, Exception $previous = null)
+    #[Pure] public function __construct($message, $code = 0, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
 
-    public function __toString()
+    public function __toString(): string
     {
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
