9ced2245fb70f8e9602375575d1e34e8b27377a9
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-02-02 11:11:00 +0100

    Change namespace to braced.

--- src/Service/Common/HttpMessagingService.php
+++ src/Service/Common/HttpMessagingService.php
@@ -1,76 +1,77 @@
 <?php declare(strict_types=1);
 
-namespace App\Service\Common;
+namespace App\Service\Common {
 
-use App\Api\Common\HttpClientInterface;
-use App\Api\Common\MessagingServiceInterface;
-use App\Api\Exceptions\ErrorCodes;
-use App\Api\Exceptions\MessagingException;
-use App\Api\Service\Parameters\MessagingParameters;
-use App\Dto\Messaging\Inner\Message;
-use App\Dto\Messaging\MessageRequest;
-use App\Dto\Messaging\MessagingResult;
-use Exception;
-
-/**
- * Service to send messages to the AR.
- * @package App\Service\Common
- * @template-implements MessagingServiceInterface<MessagingParameters>
- */
-class HttpMessagingService implements MessagingServiceInterface
-{
-    private HttpClientInterface $httpClient;
+    use App\Api\Common\HttpClientInterface;
+    use App\Api\Common\MessagingServiceInterface;
+    use App\Api\Exceptions\ErrorCodes;
+    use App\Api\Exceptions\MessagingException;
+    use App\Api\Service\Parameters\MessagingParameters;
+    use App\Dto\Messaging\Inner\Message;
+    use App\Dto\Messaging\MessageRequest;
+    use App\Dto\Messaging\MessagingResult;
+    use Exception;
 
     /**
-     * Constructor.
-     * @param HttpClientInterface $httpClient -
+     * Service to send messages to the AR.
+     * @package App\Service\Common
+     * @template-implements MessagingServiceInterface<MessagingParameters>
      */
-    public function __construct(HttpClientInterface $httpClient)
+    class HttpMessagingService implements MessagingServiceInterface
     {
-        $this->httpClient = $httpClient;
-    }
+        private HttpClientInterface $httpClient;
 
-    /**
-     * Send message to the AR using the given message parameters.
-     * @param MessagingParameters $parameters Messaging parameters.
-     * @return MessagingResult -
-     * @throws MessagingException Will be thrown in case of an error.
-     */
-    public function send($parameters): MessagingResult
-    {
-        $messageRequest = new MessageRequest();
-        $messageRequest->setSensorAlternateId($parameters->getOnboardResponse()->getSensorAlternateId());
-        $messageRequest->setCapabilityAlternateId($parameters->getOnboardResponse()->getCapabilityAlternateId());
-
-        $messages = [];
-        foreach ($parameters->getEncodedMessages() as $encodedMessage) {
-            $message = new Message();
-            $message->setContent($encodedMessage);
-            $message->setTimestamp(UtcDataService::nowAsUnixTimestamp());
-            array_push($messages, $message);
+        /**
+         * Constructor.
+         * @param HttpClientInterface $httpClient -
+         */
+        public function __construct(HttpClientInterface $httpClient)
+        {
+            $this->httpClient = $httpClient;
         }
-        $messageRequest->setMessages($messages);
 
-        $requestBody = json_encode($messageRequest);
-        $headers = [
-            'Content-type' => 'application/json',
-        ];
+        /**
+         * Send message to the AR using the given message parameters.
+         * @param MessagingParameters $parameters Messaging parameters.
+         * @return MessagingResult -
+         * @throws MessagingException Will be thrown in case of an error.
+         */
+        public function send($parameters): MessagingResult
+        {
+            $messageRequest = new MessageRequest();
+            $messageRequest->setSensorAlternateId($parameters->getOnboardResponse()->getSensorAlternateId());
+            $messageRequest->setCapabilityAlternateId($parameters->getOnboardResponse()->getCapabilityAlternateId());
 
-        $request = $this->httpClient->createRequest('POST', $parameters->getOnboardResponse()->getConnectionCriteria()->getMeasures(), $headers, $requestBody);
+            $messages = [];
+            foreach ($parameters->getEncodedMessages() as $encodedMessage) {
+                $message = new Message();
+                $message->setContent($encodedMessage);
+                $message->setTimestamp(UtcDataService::nowAsUnixTimestamp());
+                array_push($messages, $message);
+            }
+            $messageRequest->setMessages($messages);
 
-        try {
-            $response = $this->httpClient->sendAsync($request,
-                [
-                    'cert' => [CertificateService::createCertificateFile($parameters->getOnboardResponse()), $parameters->getOnboardResponse()->getAuthentication()->getSecret()],
-                    'ssl_key' => [CertificateService::createCertificateFile($parameters->getOnboardResponse()), $parameters->getOnboardResponse()->getAuthentication()->getSecret()]
-                ]);
+            $requestBody = json_encode($messageRequest);
+            $headers = [
+                'Content-type' => 'application/json',
+            ];
 
-            return json_decode($response->getBody(), true);
-        } catch (Exception $exception) {
-            if ($exception->getCode() == 400) {
-                throw new MessagingException($exception->getMessage(), ErrorCodes::INVALID_MESSAGE);
-            } else {
-                throw new MessagingException($exception->getMessage(), ErrorCodes::UNDEFINED);
+            $request = $this->httpClient->createRequest('POST', $parameters->getOnboardResponse()->getConnectionCriteria()->getMeasures(), $headers, $requestBody);
+
+            try {
+                $response = $this->httpClient->sendAsync($request,
+                    [
+                        'cert' => [CertificateService::createCertificateFile($parameters->getOnboardResponse()), $parameters->getOnboardResponse()->getAuthentication()->getSecret()],
+                        'ssl_key' => [CertificateService::createCertificateFile($parameters->getOnboardResponse()), $parameters->getOnboardResponse()->getAuthentication()->getSecret()]
+                    ]);
+
+                return json_decode($response->getBody(), true);
+            } catch (Exception $exception) {
+                if ($exception->getCode() == 400) {
+                    throw new MessagingException($exception->getMessage(), ErrorCodes::INVALID_MESSAGE);
+                } else {
+                    throw new MessagingException($exception->getMessage(), ErrorCodes::UNDEFINED);
+                }
             }
         }
     }
