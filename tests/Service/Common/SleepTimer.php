3b043803a4b9988efa17f0ed5ad87b6573d1beb1
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-02-16 12:09:44 +0100

    Correct format. Remove empty lines.

--- tests/Service/Common/SleepTimer.php
+++ tests/Service/Common/SleepTimer.php
@@ -28,9 +28,9 @@ namespace Lib\Tests\Service\Common {
          * @param int $seconds Seconds to sleep.
          * @param MqttClient|null $mqttClient
          * @return bool Was the loop interrupted by a callback handler.
-         * @throws DataTransferException -
-         * @throws MqttClientException -
-         * @throws ProtocolViolationException -
+         * @throws DataTransferException .
+         * @throws MqttClientException .
+         * @throws ProtocolViolationException .
          */
         public static function letTheAgrirouterProcessTheMqttMessage(int $seconds = 3, MqttClient $mqttClient = null): bool
         {
