fcb72ae5a2c732661340885f3c5f78468d0f6737
Ivan Hermanov <iwan.germanow@gmail.com>
2021-07-28 15:35:48 +0300

    Set default array to OutboxResponse messages in order to fix error accessing before initialization (#43)

--- src/Dto/Messaging/Http/OutboxResponse.php
+++ src/Dto/Messaging/Http/OutboxResponse.php
@@ -12,7 +12,7 @@ namespace App\Dto\Messaging\Http {
     class OutboxResponse implements JsonDeserializableInterface
     {
         private string $statusCode;
-        private array $messages;
+        private array $messages = [];
 
         public function jsonDeserialize(array|string $jsonData): JsonDeserializableInterface
         {
